3f0a28fc33ca31cc9dc58470f258dd48c2556c41
ones1kk <ones1k95@gmail.com>
2022-06-17 00:33:34 +0900

    ♻️ Refactor : Member, Team Entity

---
+++ src/main/java/study/datajpa/entity/Team.java
@@ -0,0 +1,30 @@
+package study.datajpa.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static lombok.AccessLevel.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+    private String name;
+    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL)
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+}
