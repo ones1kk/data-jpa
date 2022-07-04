885b7c76b567847adc0412fdcd023cd6c58f97b2
ones1kk <ones1k95@gmail.com>
2022-07-05 01:22:41 +0900

    ♻️ Refactor : Advanced Query Methods

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
@@ -14,5 +15,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     //    @Query(name = "Member.findByUsername")
     List<Member> findByUsername(@Param("username") String username);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -108,7 +108,22 @@ class MemberRepositoryTest {
         List<Member> result = memberRepository.findByUsername("AAA");
 
         assertThat(result.get(0).getUsername()).isEqualTo("AAA");
-        assertThat(result.get(0).getAge()).isEqualTo(20);
+        assertThat(result.get(0).getAge()).isEqualTo(10);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    @Test
+    void test6() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findUser("AAA", 10);
+
+        assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        assertThat(result.get(0).getAge()).isEqualTo(10);
         assertThat(result.size()).isEqualTo(1);
     }
 
