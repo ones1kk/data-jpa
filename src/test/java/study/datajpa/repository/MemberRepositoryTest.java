69e1ee609bf8a80cf4a62d1f4da139f7fe1c2a3a
ones1kk <ones1k95@gmail.com>
2022-07-05 01:36:28 +0900

    ✅ Add : @Query > Select DTO

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 import java.util.List;
 
@@ -19,6 +21,9 @@ class MemberRepositoryTest {
 
     @Autowired
     MemberRepository memberRepository;
+
+    @Autowired
+    TeamRepository teamRepository;
 
     @Test
     public void test1() throws Exception {
@@ -125,6 +130,35 @@ class MemberRepositoryTest {
         assertThat(result.get(0).getUsername()).isEqualTo("AAA");
         assertThat(result.get(0).getAge()).isEqualTo(10);
         assertThat(result.size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("Find username List")
+    void test7() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<String> result = memberRepository.findUsernameList();
+        assertThat(result.size()).isEqualTo(2);
+
+    }
+
+    @Test
+    void test8() {
+        Team teamA = new Team("teamA");
+
+        Member m1 = new Member("AAA", 10);
+        m1.setTeam(teamA);
+        memberRepository.save(m1);
+
+        List<MemberDto> result = memberRepository.findMemberDto();
+
+        assertThat(result.get(0).getUsername()).isEqualTo(m1.getUsername());
+        assertThat(result.get(0).getTeamName()).isEqualTo(teamA.getName());
+
     }
 
 }
